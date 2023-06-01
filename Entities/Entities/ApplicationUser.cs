24f2fe38b02d403ac88088e500b7ff5ab0a2566a
Pedrolustosa <pedroeternalss@gmail.com>
2023-06-01 08:11:16 -0300

    fea: Creation of APIs Category and Expenses - Part III

--- Entities/Entities/ApplicationUser.cs
+++ Entities/Entities/ApplicationUser.cs
@@ -3,8 +3,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Entities.Entities
 {
+    /// <summary>
+    /// The application user.
+    /// </summary>
     public class ApplicationUser : IdentityUser
     {
+        /// <summary>
+        /// Gets or Sets the cpf.
+        /// </summary>
         [Column("USER_CPF")]
         public string? CPF { get; set; }
     }
